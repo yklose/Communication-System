4f8e6c94a35bda781f2d0ff96f373645f6876c3a
Yannick <yannick.klose@icloud.com>
2019-05-15 14:49:44 +0200

    transmitter + plotting

---
+++ plot_channel_output.py
@@ -0,0 +1,22 @@
+# plot of output
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+datapath1 = "output.txt"
+datapath2 = "signal.txt"
+
+def plot_output(datapath):
+    f = open(datapath, 'r')
+    array = f.read().split('\n')
+    length = len(array)
+    plot_array = array[:(length-1)]
+    max_index = np.argmax(plot_array)
+    #print("Maximum at: " + str(max_index/length))
+    plot_array = list(map(float, plot_array))
+    plt.plot(plot_array)
+    plt.show()
+    #print(array)
+    
+plot_output(datapath2)   
+    
--- transmitter.py
+++ transmitter.py
@@ -2,6 +2,8 @@
 
 
 import numpy as np
+import math
+import operator
 
 datapath = "text.txt"
 
@@ -21,9 +23,8 @@ def create_codewords(datapath):
         ascii_bits = '{0:08b}'.format(ascii_int)
         
         codeword_str = codeword_str + " " + str(ascii_bits) + " "
-        
     
-    print(codeword_str)
+    #print(codeword_str)
     
     return codeword_str
     # save the file
@@ -31,22 +32,46 @@ def create_codewords(datapath):
     
 # waveform former!
 
-def waveform_former(codewords):
+def waveform_former():
     
+    # create codewords
     codewords = create_codewords(datapath)
+    codewords = codewords.split()
     
-    # root raised cosine? 
-    # per bit or whole message in once?
+    # set parameters
+    beta = 1/2
+    f_sample = 22050
+    T = 1/f_sample
     
+    num_bits = 8
+    w = [0]*(1000)              # example what is the time interval?
+    codeword = codewords[0]     # per bit or whole message in once?
+    
+    # use root raised cosine functions!
+    for i in range(num_bits):
+        c = codeword[i]
+        w_temp = [0]*(1000)
+        for j in range(-500,500):
+            t = T/500*j
+            term = (1+beta)*(t-i)/T
+            phi = 4*beta/(math.pi*math.sqrt(T))*(math.cos(term*math.pi)+(1-beta)*math.pi/(4*beta)*np.sinc(term))/(1-math.pow((4*beta*(t)/T),2))
+            w_temp[j-1] = float(codeword[i])*phi
+        w = list(map(operator.add, w,w_temp))
+
     # within certain frequency in order to go through the filter
-    signal = ...
     
+    # convert signal to txt file for output
+    signal = ""
+    for i in range(len(w)):
+        signal = signal + str(w[i]) + "\n"
+    #print(signal)
     signal_file = open("signal.txt", "w")
     signal_file.write(signal)
     signal_file.close()
 
 
 #to do: plot the recieved signal!
-    
+ 
+waveform_former()
 
     
